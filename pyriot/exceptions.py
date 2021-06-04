d281391a290355ceb122e638a9a7f120f438feae
Anh Pham <anhphamduy@outlook.com>
2021-06-04 18:42:50 +1000

    Add SummonerNotFound exception and DataclassMappingMismatchException

--- pyriot/exceptions.py
+++ pyriot/exceptions.py
@@ -3,3 +3,12 @@ class RiotRequestException(Exception):
         super().__init__(message)
 
         self.response = response
+
+
+class SummonerNotFound(Exception):
+    pass
+
+
+class DataclassMappingMismatchException(Exception):
+    def __init__(self, dataclass, actual_annotations):
+        super().__init__(f'dataclass {dataclass.__annotations__} does not match ${actual_annotations}')
